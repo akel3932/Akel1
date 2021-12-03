f5e12cd22c1354b29dcf1d3f4e90bcfd49b5550c
akel3932 <89228460+akel3932@users.noreply.github.com>
2021-12-03 15:44:45 -0500

    a

---
+++ src/test/java/stepDefinations/openMRSstepDef.java
@@ -0,0 +1,33 @@
+package stepDefinations;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class openMRSstepDef {
+
+    @Given("I am logged in to OpenMRS with following credentials:")
+    public void i_am_logged_in_to_open_mrs_with_following_credentials(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+
+    }
+
+    @When("I register a new patient with following info:")
+    public void i_register_a_new_patient_with_following_info(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+
+    }
+
+
+}
